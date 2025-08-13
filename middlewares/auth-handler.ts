c1ef1c821fa5b577e503c03b8f05f279bc0769f8
Delali Tengue <delalitengue.dev@gmail.com>
2025-08-13 07:00:22 +0000

    made edits

--- middlewares/auth-handler.ts
+++ middlewares/auth-handler.ts
@@ -7,13 +7,18 @@ const authFailResponse = {
 }
 
 export default async function authHandler(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(' ')[1];
-  if (!token) return res.status(401).json(authFailResponse);
-  
-  const { data, error } = await client(token).auth.getClaims(token);
-  
-  if(error) return res.status(401).json(authFailResponse);
+  try {
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.split(' ')[1];
+    if (!token) return res.status(401).json(authFailResponse);
+    
+    const { error } = await client(token).auth.getClaims(token);
 
-  next();
+    if(error) throw error;
+
+    next();
+
+  }catch(error) {
+    res.status(400).json(error);
+  }
 };
